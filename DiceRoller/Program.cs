e673c5cc1f0ab91b1d553f3c4fc0fc4cb03d092e
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-19 15:34:06 -0500

    initial commit, rolls one die

---
+++ DiceRoller/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+namespace DiceRoller
+
+{
+    internal class Program
+    {
+        // get the program to roll two dice until getting two of a kind
+        static void Main(string[] args)
+        {
+            Random numberGen = new Random();
+
+            int roll = 0;
+            int attempts = 0;
+
+            WriteLine("Press any key to roll the die...");
+
+            while (roll != 6)
+            {
+                ReadKey();
+
+                roll = numberGen.Next(1, 7);
+                WriteLine("You rolled: " + roll);
+                attempts++;
+            }
+
+            WriteLine("It took you " + attempts + " attempts to roll a six.");
+
+
+
+
+            ReadKey();
+        }
+        
+    }
+}
